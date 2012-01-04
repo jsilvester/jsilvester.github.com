6b98d37bedaa925eb5bea8a89df796ab89fafb3c
James Silvester <silvesterorama@googlemail.com>
2012-01-04 17:57:09 -0500

    adding everything

---
+++ Teaching_files/Teaching.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120104
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Teaching_files/TeachingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
